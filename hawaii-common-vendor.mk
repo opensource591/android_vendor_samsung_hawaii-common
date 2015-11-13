c19fe6f5c362c719ef86db259d7efb5e788d7e7e
Berkant Korkmaz <berkantk3@gmail.com>
2015-11-13 23:16:14 +0200

    hawaii-common-vendor: get it at correct path

--- hawaii-common-vendor.mk
+++ hawaii-common-vendor.mk
@@ -1,4 +1,4 @@
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/samsung/hawaii-common/overlay
 
-$(call inherit-product, vendor/samsung/hawaii/hawaii-common-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/hawaii-common/hawaii-common-vendor-blobs.mk)
